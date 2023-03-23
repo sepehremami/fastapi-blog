9f05877f509fcde7b169bbc84d04e275b34be544
Sepehr Emami <sepehr7890@gmail.com>
2023-03-24 00:16:39 +0100

    posts deleted

--- backend/schema/post.py
+++ backend/schema/post.py
@@ -1,11 +1,19 @@
+from datetime import datetime
 from pydantic import BaseModel
 from schema import UserOut
 
 
 class PostCreate(BaseModel):
-
     title:str
     description:str
+
+class PostOut(PostCreate):
+
+    category_id:int | None 
+    created_at:datetime
+    user_id:int
+    class Config:
+        orm_mode = True
 
 
 class PostBase(PostCreate):
