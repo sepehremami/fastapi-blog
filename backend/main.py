0d1dbf0e81dd913b5cb4594a40646a51f459c3f2
Sepehr Emami <sepehr7890@gmail.com>
2023-03-26 16:22:08 +0200

    cors origin settings update

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.origins,
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
