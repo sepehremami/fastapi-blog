2eddc3670807956c9ef94de88634065dabe84ff0
Sepehr Emami <sepehr7890@gmail.com>
2023-03-27 17:20:23 +0200

    added image endpoints

--- backend/router/user.py
+++ backend/router/user.py
@@ -3,10 +3,12 @@ import base64
 from fastapi.responses import StreamingResponse
 from io import BytesIO
 from fastapi import UploadFile, File
+from backend.oauth2 import get_current_user
 from database.models import Image
 from router import *
 import utils
 from PIL import Image as ImagePillow
+
 
 router = APIRouter(
     tags=['Users']
@@ -17,13 +19,6 @@ router = APIRouter(
 async def get_users(request: Request , db: Session = Depends(get_db)):
     users: List[UserBase] = db.query(User).all()
     return users
-
-
-@router.get("/mypost")
-async def get_users(request: Request , db: Session = Depends(get_db)):
-    users: List[UserBase] = db.query(User).all()
-
-    return templates.TemplateResponse("myPosts.html", {"request": request, "users": users})
 
 
 @router.get("/users/{id}")
@@ -77,63 +72,22 @@ def update_hero(id: int, updated_user: UserBase,db: Session=Depends(get_db)):
     db.refresh(user)
     return user
 
-@router.post("/user/image/{user_id}")
-async def upload_image(user_id: int ,db :Session = Depends(get_db), file: UploadFile = File(...)):
-    # Create a SQLAlchemy db
+
+@router.get('/image/view')
+async def view_image(response: Response, db:Session=Depends(get_db), current_user=Depends(get_current_user)):
+    image = db.query(Image).filter(Image.user_id==current_user.id).first()
+    img = ImagePillow.open(BytesIO(image.image))
+    buffer = BytesIO()
+    img.save(buffer, format='JPEG')
+    base64_encoded_image = base64.b64encode(image.image).decode("utf-8")
+    return base64_encoded_image
+
+
+@router.post("/user/image/")
+async def upload_image(user_id: int ,db :Session = Depends(get_db), file: UploadFile = File(...), current_user=Depends(get_current_user)):
     file_content = await file.read()
-    # Update the corresponding user object with the binary data
-    image = Image(image=file_content, user_id=user_id)
-    image.user_id = user_id
+    image = Image(image=file_content, user_id=current_user.id)
     db.add(image)
     db.commit()
     db.close()
-    # Return the updated user profile
     return image
-
-@router.get('/user/image')
-async def get_images(db: Session = Depends(get_db)):
-    images = db.query(Image).all()
-
-    return images
-
-
-@router.get("/images/")
-def get_image(db:Session= Depends(get_db)):
-    # Get the row containing the image data
-    image_row = db.query(Image).first()
-    print(image_row)
-    if image_row is not None:
-        # Access the LargeBinary column containing the image data
-        image_data = image_row.image
-        print(type(image_data))
-        # Convert the LargeBinary data to a Pillow Image
-        image = ImagePillow.open(BytesIO(image_data))
-        
-        # Save the image to a BytesIO object
-        buffer = BytesIO()
-      
-        image.save(buffer, format="JPEG")
-
-        # Return the image in the response
-        return StreamingResponse(buffer, media_type="image/jpeg")
-    else:
-        raise HTTPException(status_code=404, detail="Image not found")
-    
-@router.get('/image/view')
-async def view_image(response: Response, db:Session=Depends(get_db)):
-    # Query image data from database
-    
-    image = db.query(Image).first()
-    
-
-    # Convert binary data to PIL Image
-    img = ImagePillow.open(BytesIO(image.image))
-    # Create BytesIO buffer to hold JPEG data
-    buffer = BytesIO()
-    img.save(buffer, format='JPEG')
-
-    base64_encoded_image = base64.b64encode(image.image).decode("utf-8")
-    # Return JPEG data as StreamingResponse
-    # stream = StreamingResponse(iter([buffer.getvalue()]), media_type='image/jpeg')
-    # print(stream)
-    return base64_encoded_image
