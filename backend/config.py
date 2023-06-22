46d670af83e23536ffb747872f90af8fca426c7a
Sepehr Emami <107993845+sepehremami@users.noreply.github.com>
2023-06-22 14:53:02 +0330

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,14 +2,14 @@ from pydantic import BaseSettings
 import os
 
 class Setting(BaseSettings):
-    database_hostname:str ="localhost"
-    database_port:str = "5432"
-    database_name:str= 'online_shop'
-    database_password:str = '11223'
-    database_username:str=  "postgres"
+    database_hostname:str 
+    database_port:str 
+    database_name:str
+    database_password:str
+    database_username:str
     secret_key:str
-    algorithm:str= "HS256"
-    access_tocken_expire_minutes:str = "30"
+    algorithm:str
+    access_tocken_expire_minutes:str 
     origins = ['*',
     "http://localhost:8000",
     "http://localhost:3000/*",
